91903037abba81b10f19f995f7501e4db47379e4
harika-atmala <harika.atmala@gmail.com>
2018-03-12 09:13:55 -0400

    Update cassandra.sh

--- cassandra.sh
+++ cassandra.sh
@@ -2,7 +2,7 @@
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
 sudo apt-get -y install oracle-java8-installer
-echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.list
+echo deb http://www.apache.org/dist/cassandra/debian 311x main | sudo tee -a /etc/apt/sources.list.d/cassandra.list
 curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt-get -y update
 sudo apt-get -y install cassandra
